c25a28dc1430ba22b7a2ef4fef6d8684c2133a9a
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-08-31 09:09:13 +0530

    Setter and Getter method/properties

--- javascript/class-concept.js
+++ javascript/class-concept.js
@@ -9,55 +9,86 @@
 // new showDetails('Dinesh11','India');
 
 
-class ShowDetails{
-    constructor(name, add){
-        this.name=name;
-        this.address=add;
+// class ShowDetails{
+//     constructor(name, add){
+//         this.name=name;
+//         this.address=add;
+//     }
+//     showLog(){
+//         console.log(this);
+//     }
+//     showFullDetails(){
+//         return `${this.name} ${this.address}`;
+//     }
+// }
+
+// class Calculation{
+//     static randowNumber(){
+//         return Math.random();
+//     }
+//     getAbsoluteNumber(num){
+//         return Math.abs(num);
+//     }
+// }
+
+// // (new ShowDetails('dinesh','India')).showLog();
+// let showDetailsObj=new ShowDetails('dinesh','India');
+// showDetailsObj.showLog();
+
+
+// console.log(Calculation.randowNumber());
+// let calcObj=new Calculation();
+// console.log(calcObj.getAbsoluteNumber(-243));
+
+
+// class Counter{
+//     static count=0;
+//     constructor(){
+//         Counter.count++;
+//     }
+//     getCountValue(){
+//         return Counter.count;
+//     }
+// }
+
+// let counObj=new Counter(); //1
+// console.log(counObj.getCountValue());
+// let counObj1=new Counter(); // 2
+// console.log(counObj.getCountValue());
+// console.log(counObj1.getCountValue());
+// let counObj2=new Counter(); // 3
+// console.log(counObj1.getCountValue());
+// let counObj3=new Counter(); // 4
+// console.log(counObj.getCountValue());
+
+
+class Student {
+    _name = 'dinesh';
+    constructor(name) {
+        console.log('inside constructor', this.name);
+        this._name = name;
     }
-    showLog(){
-        console.log(this);
+
+    // getName(){
+    //     return this.name;
+    // }
+
+    // setName(newName){
+    //     this.name=newName;
+    // }
+    get name() {
+        return this._name;
     }
-    showFullDetails(){
-        return `${this.name} ${this.address}`;
+
+    set name(newName) {
+        this._name = newName;
     }
 }
-
-class Calculation{
-    static randowNumber(){
-        return Math.random();
-    }
-    getAbsoluteNumber(num){
-        return Math.abs(num);
-    }
-}
-
-// (new ShowDetails('dinesh','India')).showLog();
-let showDetailsObj=new ShowDetails('dinesh','India');
-showDetailsObj.showLog();
+let studentObj = new Student('Gopal');
+console.log(studentObj.name);
+studentObj.name = 'Chand';
+console.log(studentObj.name);
 
 
-console.log(Calculation.randowNumber());
-let calcObj=new Calculation();
-console.log(calcObj.getAbsoluteNumber(-243));
 
-
-class Counter{
-    static count=0;
-    constructor(){
-        Counter.count++;
-    }
-    getCountValue(){
-        return Counter.count;
-    }
-}
-
-let counObj=new Counter(); //1
-console.log(counObj.getCountValue());
-let counObj1=new Counter(); // 2
-console.log(counObj.getCountValue());
-console.log(counObj1.getCountValue());
-let counObj2=new Counter(); // 3
-console.log(counObj1.getCountValue());
-let counObj3=new Counter(); // 4
-console.log(counObj.getCountValue());
 
