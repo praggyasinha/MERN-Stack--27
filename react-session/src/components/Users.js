c14446825766712dad8b581d10e6a556a5705a0a
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-09-08 09:19:53 +0530

    Inline css introduced

--- react-session/src/components/Users.js
+++ react-session/src/components/Users.js
@@ -4,24 +4,39 @@ import './Users.css';
 import Card from 'react-bootstrap/Card';
 
 function Users() {
-    const userDetails = {
+    const userDetails = [{
         name: 'Manisha',
         location: 'BLR, India',
         phone: '+91-98765432190'
+    }, {
+
+        name: 'Deepa',
+        location: 'BLR, India',
+        phone: '+91-987655590'
+    }, {
+
+        name: 'Praggya',
+        location: 'BLR, India',
+        phone: '+91-999995590'
     }
+
+    ];
     const showUpperCase = str => str.toUpperCase();
+    const detailsList = userDetails.map(user => {
+        return <Card style={{ width: '100%', border: '1px solid gray', padding: '5px 10px', marginBottom: '5px', borderRadius: '5px' }}>
+            <Card.Img variant="top" className='profile-image' src={profileImg} />
+            <Card.Body>
+                <Card.Title>{user.name}</Card.Title>
+                <Card.Text>
+                    Location: {showUpperCase(user.location)} | Phone: {user.phone}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    })
 
     return (
         <div className="user-details">
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" className='profile-image' src={profileImg} />
-                <Card.Body>
-                    <Card.Title>{userDetails.name}</Card.Title>
-                    <Card.Text>
-                        Location: {showUpperCase(userDetails.location)} | Phone: {userDetails.phone}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            {detailsList}
         </div>
     );
 }
